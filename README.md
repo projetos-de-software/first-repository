456866c2bb57ae06664e2e4625d50922f647a499
Marcos de Lima Carlos <mlimacarlos@gmail.com>
2024-12-25 15:01:43 -0300

    docs: inicio da documentação

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# first-repository
-Organization Repo for Github
+# Repositório Inicial 
+
+Este repositório foi criado com o intuito de promover uma organização entre projetos aqui no [Github](https://www.github.com)
+
+## Descrição Inicial
+
+Há um problema sério no github com relação ao agrupamento de projetos. Como a ideia do projetos de software é produzir conteúdo, para que você possa se orientar com relação aos projetos eu criei este repositório. Ele possui, além da documentação básica dos repositórios, um guia para que você possa entender como funciona a estrutura dos repositórios dentro do site. 
+
+## Nomenclaturas Iniciais
+
+| Número |  Sigla  | Nível |          Descrição           |
+| :----: | :-----: | :---: | :--------------------------: |
+|   01   | joomla  |  01   |   Projetos de Joomla(PHP)    |
+|   02   | arduino |  01   | Projetos baseados em arduino |
+
+
+## Documentos
+
+| Número | Título | Descrição |
+| :----: | :----: | :-------: |
+
+
+## Scripts
+
+| Número | Nome  | Descrição |
+| :----: | :---: | :-------: |
---
+++ script/script.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+## Inicializa tudo como default
+yarn init -y 
+
+## Instalação do commitlint
+yarn add -D @commitlint/{cli,config-conventional}
+echo "module.exports = { extends: ['@commitlint/config-conventional'] };" > commitlint.config.js
+
+## instalação husky
+yarn add -D husky
+npx husky init 
+rm -rf .husky/pre-commit
+echo "npx --no -- commitlint --edit ${1}" > .husky/commit-msg
+
+## commitzen
+yarn add -D commitizen
+yarn commitizen init cz-conventional-changelog --yarn --dev --exact
+
+
+## Standard - Version
+yarn add -D standard-version
+
+echo $(jq '. += {"scripts":{}}' package.json) > package.json
+echo $(jq '.scripts +={"prepare":"husky"}' package.json) > package.json
+echo $(jq '.scripts +={"commit":"git-cz"}' package.json) > package.json
+echo $(jq '.scripts +={"release":"standard-version"}' package.json) > package.json
+
+# Versionamento Changelog
+cat <<EOF > .versionrc
+{
+  "types": [
+    { "type": "feat", "section": "Funcionalidades" },
+    { "type": "fix", "section": "Bugs" },
+    { "type": "chore", "hidden": true },
+    { "type": "docs", "hidden": true },
+    { "type": "style", "hidden": true },
+    { "type": "refactor", "hidden": true },
+    { "type": "perf", "hidden": true },
+    { "type": "test", "hidden": true },
+    { "type": "build", "hidden": true }
+  ] 
+}
+EOF
+
+## Create .Gitignore
+cat <<EOF > .gitignore
+node_modules
+EOF
